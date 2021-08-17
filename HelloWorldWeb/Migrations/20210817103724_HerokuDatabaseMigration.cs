4c17fe54f58f4d445134b024c14d675b65c4fe5b
radu9813 <radu9813@gmail.com>
2021-08-17 13:56:59 +0300

    Achieved connecting to a heroku db

---
+++ HelloWorldWeb/Migrations/20210817103724_HerokuDatabaseMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HelloWorldWeb.Migrations
+{
+    public partial class HerokuDatabaseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
