42035da0d116492b25f6189077cf9697c528011a
radu9813 <radu9813@gmail.com>
2021-08-19 09:46:55 +0300

    C# features project created and start using Serializationa adn Deserialization

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // TimeService timeService = new TimeService();
+            TeamMember teamMember = new TeamMember();
+            teamMember.Name = "Radu";
+            string jsonString = JsonSerializer.Serialize(teamMember);
+            Console.WriteLine(jsonString);
+            File.WriteAllText("TeamMember.json", jsonString);
+
+            var teamMember2 = JsonSerializer.Deserialize<TeamMember>(jsonString);
+        }
+    }
+}
--- HelloWorldWebApp/Program.cs
+++ HelloWorldWebApp/Program.cs
@@ -26,5 +26,5 @@ namespace HelloWorldWebApp
                     webBuilder.UseStartup<Startup>()
                     .UseUrls("http://*:" + port);
                 });
+        }
     }
-}
