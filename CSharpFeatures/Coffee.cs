7daeff27df2a3936f84a2cca589910c8808a60ae
radu9813 <radu9813@gmail.com>
2021-08-19 11:27:09 +0300

    Coffee Dispenser implemented + exception handling

---
+++ CSharpFeatures/Coffee.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpFeatures
+{
+    internal class Coffee
+    {
+      
+
+        public Coffee(string coffeType)
+        {
+            this.CoffeeType = coffeType;
+        }
+
+        public string CoffeeType { get; set; }
+
+        public override string ToString()
+        {
+            return CoffeeType;
+        }
+    }
+}
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -20,7 +20,56 @@ namespace CSharpFeatures
             var json = readText.Result;
             var teamMember2 = JsonSerializer.Deserialize<TeamMember>(json);
 
+            
             Console.WriteLine(teamMember2.ToString());
+            Console.Write("What would you like: ");
+            var input = Console.ReadLine();
+            Func<string, string, string, string, Coffee> recipe = (input=="FlatWhite") ? FlatWhite : Espresso;
+        
+            Coffee coffee = MakeCoffee("grains", "cow", "2 cups", "2 tablespoons", recipe);
+
+            if (coffee != null)
+            {
+                Console.WriteLine($"Here is your coffee {coffee}");
+            }
+            else
+            {
+                Console.WriteLine("We are sorry that we could not deliver your coffee.");
+            }
+            
+        }
+
+        static Coffee MakeCoffee(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffee> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffee.");
+
+                var coffee = recipe(grains, milk, water, sugar);
+                return coffee;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry your order could not be completed");
+                return null;
+                
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+       
+
+        }
+
+        static Coffee Espresso(string grains, string milk, string water, string sugar)
+        {
+            throw new RecipeUnavailableException();
+        }
+
+        static Coffee FlatWhite(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("FlatWhite");
         }
     }
 }
